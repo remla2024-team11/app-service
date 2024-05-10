09bc317792cd533a24c3290cb7655ddd5159a41e
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2024-05-10 15:09:41 +0200

    Add ENV for apis and origins

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+MODEL_API = os.getenv("MODEL_API")
+FRONTEND= os.getenv("FRONTEND")
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/api/*": {"origins": FRONTEND}})
 
 # Dummy database to store data
 data = []
@@ -14,7 +21,7 @@ def add_data():
     try:
         new_data = request.json
         
-        response = requests.post('https://jsonplaceholder.typicode.com/posts', json=new_data)
+        response = requests.post(MODEL_API, json=new_data)
         print(response.json())
         if response.status_code == 200 or response.status_code == 201:
             data.append(new_data)
